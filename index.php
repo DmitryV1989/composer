47590fd454ac8a059ab87dafc6a6e69c1f3bc3b3
DmitryV1989 <Kleeper@mail.ru>
2024-01-21 19:31:03 +0300

    init

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . "/vendor/autoload.php";
+
+//dump("Hello");
+
+$user = new user;
+dump($user->digit);
+
+// TODO: выработать .gitignore, загнать в репозиторий , деплой на продакшене , после деплоя заново установить пакеты
